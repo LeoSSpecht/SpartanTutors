0a7ac8b75b02329b29b16af4b2a379de152f30f0
Leo <leo.s.specht@gmail.com>
2022-06-11 15:12:25 -0300

    Changed hello

---
+++ SpartanTutors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpartanTutors
+//
+//  Created by Leo on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, leo!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpartanTutors/SpartanTutorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpartanTutorsApp.swift
+//  SpartanTutors
+//
+//  Created by Leo on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SpartanTutorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
